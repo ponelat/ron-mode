54f6819f6047e29d28b4672953036df92c964b25
Josh Ponelat <jponelat@gmail.com>
2019-09-15 22:27:59 +0200

    Updates

---
+++ precache-manifest.ae02c8cf02ff6e67ee96ebbf7d020ae3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32e05af3a44741a5e35bf7b5f712e91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1777d0521d1e72194525",
+    "url": "/static/css/2.a8e4c19d.chunk.css"
+  },
+  {
+    "revision": "be2a56db8755e7aa88b9",
+    "url": "/static/css/main.53a05c60.chunk.css"
+  },
+  {
+    "revision": "1777d0521d1e72194525",
+    "url": "/static/js/2.d33f14b7.chunk.js"
+  },
+  {
+    "revision": "be2a56db8755e7aa88b9",
+    "url": "/static/js/main.9eb6aa88.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c89f593b4cfe4ee82a8b2d99361e7d48",
+    "url": "/static/media/AmazDooMLeft.c89f593b.ttf"
+  },
+  {
+    "revision": "8b6d3979fc9b12e6d40211e99510f8cb",
+    "url": "/static/media/AmazDooMRight.8b6d3979.ttf"
+  },
+  {
+    "revision": "9d9effb54305d963d9cdf680aa9db119",
+    "url": "/static/media/doom-guy-sprite.9d9effb5.png"
+  }
+]);
